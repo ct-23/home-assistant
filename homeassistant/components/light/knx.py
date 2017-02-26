21795d2dc4ff8c6f2c261bf57ffc811ce37f68e8
Christian T <dacrush0r@gmail.com>
2017-02-26 22:03:43 +0100

    Flexible KNX

--- homeassistant/components/light/knx.py
+++ homeassistant/components/light/knx.py
@@ -1,77 +1,81 @@
 """
-Support KNX Lighting actuators.
+Support KNX switching actuators.
 
 For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/Light.knx/
+https://home-assistant.io/components/switch.knx/
 """
-import logging
+
 import voluptuous as vol
 
-from homeassistant.components.knx import (KNXConfig, KNXMultiAddressDevice)
-from homeassistant.components.light import (Light, PLATFORM_SCHEMA,
+from homeassistant.components.knx import (KNXConfig, KNXFlexibleEntity)
+from homeassistant.components.light import (Light,
+                                            PLATFORM_SCHEMA,
                                             SUPPORT_BRIGHTNESS,
                                             ATTR_BRIGHTNESS)
-from homeassistant.const import CONF_NAME
+from homeassistant.const import (CONF_NAME,
+                                 CONF_BRIGHTNESS)
 import homeassistant.helpers.config_validation as cv
 
-CONF_ADDRESS = 'address'
-CONF_STATE_ADDRESS = 'state_address'
-CONF_BRIGHTNESS_ADDRESS = 'brightness_address'
-CONF_BRIGHTNESS_STATE_ADDRESS = 'brightness_state_address'
+from typing import Optional
 
-_LOGGER = logging.getLogger(__name__)
+ON_OFF_BLOCK = 'on_off'
 
 DEFAULT_NAME = 'KNX Light'
 DEPENDENCIES = ['knx']
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_ADDRESS): cv.string,
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_STATE_ADDRESS): cv.string,
-    vol.Optional(CONF_BRIGHTNESS_ADDRESS): cv.string,
-    vol.Optional(CONF_BRIGHTNESS_STATE_ADDRESS): cv.string,
+ga_item = vol.Schema({
+    vol.Required("address"): str,
+    vol.Optional("state_address"): str,
 })
 
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(ON_OFF_BLOCK): ga_item,
+    vol.Optional(CONF_BRIGHTNESS): ga_item,
+    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+})
 
-def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Set up the KNX light platform."""
-    add_devices([KNXLight(hass, KNXConfig(config))])
+DEFAULT_BRIGHTNESS = b'\x00'
+
+SUPPORT_KNX = (SUPPORT_BRIGHTNESS)
 
 
-class KNXLight(KNXMultiAddressDevice, Light):
-    """Representation of a KNX Light device."""
+def setup_platform(hass, config: dict, add_devices, discovery_info=None):
+    """Setup the KNX switch platform."""
+    add_devices([KNXLight(hass, config)])
+
+
+class KNXLight(KNXFlexibleEntity, Light):
+    """Representation of a KNX light device."""
 
     def __init__(self, hass, config):
-        """Initialize the cover."""
-        KNXMultiAddressDevice.__init__(
-            self, hass, config,
-            [],  # required
-            optional=['state', 'brightness', 'brightness_state']
-        )
-        self._hass = hass
-        self._supported_features = 0
-
-        if CONF_BRIGHTNESS_ADDRESS in config.config:
-            _LOGGER.debug("%s is dimmable", self.name)
-            self._supported_features = self._supported_features | \
-                SUPPORT_BRIGHTNESS
-            self._brightness = None
-
-    def turn_on(self, **kwargs):
-        """Turn the switch on.
-
-        This sends a value 1 to the group address of the device
         """
-        _LOGGER.debug("%s: turn on", self.name)
-        self.set_value('base', [1])
-        self._state = 1
+        Initialize the device using a config Map.
 
+        KNX relevant Configuration is converted to KNXConfig
+        and passed to Parent
+        """
+        light_settings = {ON_OFF_BLOCK: KNXConfig(config[ON_OFF_BLOCK])}
+        if config.get(CONF_BRIGHTNESS) is not None:
+            light_settings.update({
+                CONF_BRIGHTNESS: KNXConfig(config[CONF_BRIGHTNESS])
+            })
+        self.__name = config.get(CONF_NAME)
+        super().__init__(hass, light_settings)
+
+    @property
+    def name(self) -> Optional[str]:
+        """Return the name of the entity."""
+        return self.__name
+
+    def turn_on(self, **kwargs) -> None:
+        """Turn the Light on.
+
+        This either sets brightness or turns on the device.
+        """
         if ATTR_BRIGHTNESS in kwargs:
-            self._brightness = kwargs[ATTR_BRIGHTNESS]
-            _LOGGER.debug("turn_on requested brightness for light: %s is: %s ",
-                          self.name, self._brightness)
-            assert self._brightness <= 255
-            self.set_value("brightness", [self._brightness])
+            self.address[CONF_BRIGHTNESS].group_write(kwargs[ATTR_BRIGHTNESS])
+        else:
+            self.address[ON_OFF_BLOCK].group_write(1)
 
         if not self.should_poll:
             self.schedule_update_ha_state()
@@ -81,36 +85,32 @@ class KNXLight(KNXMultiAddressDevice, Light):
 
         This sends a value 1 to the group address of the device
         """
-        _LOGGER.debug("%s: turn off", self.name)
-        self.set_value('base', [0])
-        self._state = 0
+        self.address[ON_OFF_BLOCK].group_write(0)
         if not self.should_poll:
             self.schedule_update_ha_state()
 
     @property
     def is_on(self):
-        """Return True if the value is not 0 is on, else False."""
-        return self._state != 0
+        """Return True if the value is not 0 is on, else False.
+
+        [0] is the representation from the KNX library.
+        """
+        return self.raw_state.get(ON_OFF_BLOCK) != [0]
+
+    @property
+    def brightness(self):
+        """Return the brightness of this light between 0..255."""
+        return int.from_bytes(
+            self.raw_state.get(CONF_BRIGHTNESS, DEFAULT_BRIGHTNESS),
+            byteorder='big'
+        )
 
     @property
     def supported_features(self):
         """Flag supported features."""
-        return self._supported_features
+        default_features = 0
+        supported_features = default_features
+        if self.address.get(CONF_BRIGHTNESS) is not None:
+            supported_features = supported_features | SUPPORT_BRIGHTNESS
 
-    def update(self):
-        """Update device state."""
-        super().update()
-        if self.has_attribute('brightness_state'):
-            value = self.value('brightness_state')
-            if value is not None:
-                self._brightness = int.from_bytes(value, byteorder='little')
-                _LOGGER.debug("%s: brightness = %d",
-                              self.name, self._brightness)
-
-        if self.has_attribute('state'):
-            self._state = self.value("state")[0]
-            _LOGGER.debug("%s: state = %d", self.name, self._state)
-
-    def should_poll(self):
-        """No polling needed for a KNX light."""
-        return False
+        return supported_features
